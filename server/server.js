3b61fd1f144d5f1de92cdd1cbf3efc06a5961ecd
Dushyant Khandelwal <dushyantkhandelwal4665@gmail.com>
2024-10-16 14:06:53 +0530

    add

--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const authRoutes = require('../routes/authRoutes'); // Ensure this path is correct
-const taskRoutes = require('../routes/taskRoutes'); // Ensure this path is also correct
+const authRoutes = require('../routes/authRoutes'); 
+const taskRoutes = require('../routes/taskRoutes'); 
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const app = express();
-
 app.use(bodyParser.json());
-
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
