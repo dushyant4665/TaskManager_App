2b8293d2b38adb479b5a6e2cc28833d7d01966d2
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-10-24 16:50:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -31,19 +31,19 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux'; // Import Provider from react-redux
-import store from './redux/store'; // Import the Redux store
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Home from './components/Auth/home';
 import Register from './components/Auth/register';
 import Login from './components/Auth/login';
 import TaskForm from './components/taskForm';
-import TaskList from './components/taskList'; // Add TaskList component
-import TaskItem from './components/taskItem'; // Add TaskItem component
+import TaskList from './components/taskList';
+import TaskItem from './components/taskItem';
 import Header from './components/Layout/header';
 
 const App = () => {
   return (
-    <Provider store={store}> {/* Wrap the app in the Provider and pass the store */}
+    <Provider store={store}>
       <Router>
         <div className="flex flex-col min-h-screen">
           <Header />
