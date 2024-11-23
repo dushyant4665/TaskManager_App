643c62168d728fafe97143722b43b671894f5ce7
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-11-24 01:51:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './components/Auth/home'; 
-// import Register from './components/Auth/register';
-// import Login from './components/Auth/login'; 
-// import Footer from './components/Layout/footer';
-// import Header from './components/Layout/header'; 
-// import TaskForm from './components/taskForm'; 
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="flex flex-col min-h-screen">
-//         <Header /> 
-//         <main className="flex-grow">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/register" element={<Register />} />
-//             <Route path="/login" element={<Login />}/>
-//             <Route path="/create-task" element={<TaskForm />} /> 
-//           </Routes>
-//         </main>
-//         {/* <Footer /> */}
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
 
 
 import React from 'react';
@@ -42,7 +14,7 @@ import TaskItem from './components/taskItem';
 import Header from './components/Layout/header';
 import Footer from './components/Layout/footer';
 
-const App = () => {
+const App=()=>{
   return (
     <Provider store={store}>
       <Router>
@@ -50,12 +22,12 @@ const App = () => {
           <Header />
           <main className="flex-grow">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/"element={<Home />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
               <Route path="/create-task" element={<TaskForm />} />
               <Route path="/tasks" element={<TaskList />} /> 
-              <Route path="/tasks/:id" element={<TaskItem />} />
+              <Route path="/tasks/:id"element={<TaskItem />} />
            
             </Routes>
           </main>
